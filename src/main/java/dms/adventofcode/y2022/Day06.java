c07fea03e9e67a70641a77071dbecf929d1c0404
dsirobokov <dsirobokov@anwb.nl>
2022-12-06 14:40:39 +0100

    Year 2022 / Day 6

--- src/main/java/dms/adventofcode/y2022/Day06.java
+++ src/main/java/dms/adventofcode/y2022/Day06.java
@@ -11,32 +11,57 @@ public class Day06 {
 
     public static int findMessageIndex(List<String> input) {
         var line = input.get(0);
-        Integer i = findMarkerIndex(line, 14);
-        if (i != null) return i;
-        return -1;
+        return findMarkerIndex(line, 14);
     }
+
+    private Day06() { }
 
     private static int findMarkerIndex(String line, int len) {
-        for (var i = 0; i < line.length() - len; i++) {
-            if (isMarker(line, i, len)) {
-                return i + len;
+        var markerWindow = new UniqueMarkerWindow(len);
+        for (var i = 0; i < line.length(); i++) {
+            if (markerWindow.add(line.charAt(i))) {
+                return i + 1;
             }
         }
         return -1;
     }
 
-    private static boolean isMarker(String line, int pos, int len) {
-        if (pos + len > line.length()) {
-            return false;
+    private static class UniqueMarkerWindow {
+        private final char[] window;
+        private final int[] counters;
+
+        private int pos;
+
+        private int counter;
+
+        /**
+         * @param size Size of the window
+         */
+        public UniqueMarkerWindow(int size) {
+            window = new char[size];
+            counters = new int[256];
+            counters[0] = size + 1;
+            counter = 1;
         }
-        for (var i = pos; i < pos + len - 1; i++) {
-            for (var j = i + 1; j < pos + len; j++) {
-                if (line.charAt(i) == line.charAt(j)) {
-                    return false;
-                }
-            }
+
+        /** add a character to a window, and return true if the sequence in window is unique.
+         * @param ch - a character to add to a window
+         * @return - return true if the sequence in window is unique.
+         */
+        public boolean add(char ch) {
+            var tailCharacterCount = --counters[window[pos]];
+            if (tailCharacterCount == 1) counter--;
+
+            window[pos] = ch;
+
+            var headCharacterCount = ++counters[ch];
+            if (headCharacterCount == 2) counter++;
+
+            this.pos++;
+            if (this.pos == window.length) this.pos = 0;
+
+            return counter == 0;
         }
-        return true;
     }
 
 }
