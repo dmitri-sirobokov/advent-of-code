e96974b908556baa666e2eeeff61af7e94e6dcc5
dsirobokov <dsirobokov@anwb.nl>
2022-12-13 18:14:52 +0100

    Year 2021 / Day 16

--- src/main/java/dms/adventofcode/y2021/Day16.java
+++ src/main/java/dms/adventofcode/y2021/Day16.java
@@ -10,6 +10,46 @@ public class Day16 extends CodeBase {
 
     public static long part1(List<String> input) throws IOException {
 
+        Packet packet = readPacket(input);
+
+        return countPacketsVersionNumbers(packet);
+    }
+
+    public static long part2(List<String> input) {
+        Packet packet = readPacket(input);
+
+        return calcValue(packet);
+    }
+
+    private static long countPacketsVersionNumbers(Packet packet) {
+        var sum = packet.packetVersion;
+        for (var subPacket : packet.subPackets) {
+            sum += countPacketsVersionNumbers(subPacket);
+        }
+        return sum;
+    }
+
+    private static long calcValue(Packet packet) {
+        if (packet instanceof LiteralPacket lp) {
+            return lp.literal;
+        }
+
+        if (packet instanceof OperatorPacket op) {
+            return switch (Long.valueOf(op.packetTypeId).intValue()) {
+                case 0 -> op.subPackets.stream().mapToLong(p -> calcValue(p)).sum();
+                case 1 -> op.subPackets.stream().mapToLong(p -> calcValue(p)).reduce((a,b) -> a * b).orElse(0);
+                case 2 -> op.subPackets.stream().mapToLong(p -> calcValue(p)).min().orElse(0);
+                case 3 -> op.subPackets.stream().mapToLong(p -> calcValue(p)).max().orElse(0);
+                case 5 -> (calcValue(op.subPackets.get(0)) > calcValue(op.subPackets.get(1))) ? 1L : 0L;
+                case 6 -> (calcValue(op.subPackets.get(0)) < calcValue(op.subPackets.get(1))) ? 1L : 0L;
+                case 7 -> (calcValue(op.subPackets.get(0)) == calcValue(op.subPackets.get(1))) ? 1L : 0L;
+                default -> throw new IllegalStateException("Unexpected value: " + Long.valueOf(op.packetTypeId));
+            };
+        }
+        return 0;
+    }
+
+    private static Packet readPacket(List<String> input) {
         var binaryStringBuilder = new StringBuilder(input.get(0).length() * 4);
         for (var i = 0; i < input.get(0).length(); i++) {
             var decimal = Integer.parseInt(String.valueOf(input.get(0).charAt(i)), 16);
@@ -18,9 +58,7 @@ public class Day16 extends CodeBase {
 
 
         var binaryStringReader = new BinaryStringReader(binaryStringBuilder);
-        var packet = readPacket(binaryStringReader);
-
-        return 0;
+        return readPacket(binaryStringReader);
     }
 
     private static Packet readPacket(BinaryStringReader binaryStringReader) {
@@ -31,14 +69,26 @@ public class Day16 extends CodeBase {
             var literal = readLiteral(binaryStringReader);
             return new LiteralPacket(packetVersion, packetTypeId, literal);
         } else {
-            // operator packet
+            var operatorPacket = new OperatorPacket(packetVersion, packetTypeId);
             var lengthTypeID = binaryStringReader.readBits(1);
             var length = lengthTypeID == 0 ? binaryStringReader.readBits(15) : binaryStringReader.readBits(11);
             if (lengthTypeID == 0) {
-
+                // length type is number of bits
+                var subPacketData = binaryStringReader.readString(Long.valueOf(length).intValue());
+                var subPacketStringReader = new BinaryStringReader(new StringBuilder(subPacketData));
+                while (subPacketStringReader.next < subPacketStringReader.length) {
+                    var subPacket = readPacket(subPacketStringReader);
+                    operatorPacket.subPackets.add(subPacket);
+                }
+            } else if (lengthTypeID == 1) {
+                // length type is number of packets
+                for (var i = 0; i < length; i++) {
+                    var subPacket = readPacket(binaryStringReader);
+                    operatorPacket.subPackets.add(subPacket);
+                }
             }
+            return operatorPacket;
         }
-        return null;
     }
 
     private static long readLiteral(BinaryStringReader binaryStringReader) {
@@ -54,16 +104,14 @@ public class Day16 extends CodeBase {
         return literalValue;
     }
 
-    public static long part2(List<String> input) {
-        return 0;
-    }
-
     private static class BinaryStringReader {
         private final StringBuilder binaryString;
+        private final int length;
         private int next = 0;
 
         public BinaryStringReader(StringBuilder binaryString) {
             this.binaryString = binaryString;
+            this.length = binaryString.length();
         }
 
         public long readBits(int num) {
@@ -74,15 +122,21 @@ public class Day16 extends CodeBase {
             return result;
         }
 
+        public String readString(int num) {
+            var result = binaryString.substring(next, next + num);
+            next += num;
+            return result;
+        }
+
         public boolean hasMore() {
             return next < binaryString.length();
         }
     }
 
     private static sealed class Packet permits LiteralPacket, OperatorPacket {
-        private final List<Packet> subPackets = new ArrayList<>();
-        private final long packetVersion;
-        private final long packetTypeId;
+        public final List<Packet> subPackets = new ArrayList<>();
+        public final long packetVersion;
+        public final long packetTypeId;
 
         public Packet(long packetVersion, long packetTypeId) {
             this.packetVersion = packetVersion;
--- src/test/java/dms/adventofcode/y2021/Day16Test.java
+++ src/test/java/dms/adventofcode/y2021/Day16Test.java
@@ -1,7 +1,6 @@
 package dms.adventofcode.y2021;
 
 import dms.adventofcode.TestInput;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 
 import java.io.IOException;
@@ -12,20 +11,32 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
  * Day 16: Packet Decoder
  */
-@Disabled("Under construction")
 class Day16Test {
 
     @ParameterizedTest
-    @TestInput(input = "y2021/day16_sample.txt", expected = "1")
-    @TestInput(input = "y2021/day16.txt", expected = "1")
+    @TestInput(input = "y2021/day16_sample1.txt", expected = "6")
+    @TestInput(input = "y2021/day16_sample2.txt", expected = "9")
+    @TestInput(input = "y2021/day16_sample3.txt", expected = "14")
+    @TestInput(input = "y2021/day16_sample4.txt", expected = "16")
+    @TestInput(input = "y2021/day16_sample5.txt", expected = "12")
+    @TestInput(input = "y2021/day16_sample6.txt", expected = "23")
+    @TestInput(input = "y2021/day16_sample7.txt", expected = "31")
+    @TestInput(input = "y2021/day16.txt", expected = "938")
     void part1(List<String> input, long expected) throws IOException {
         var result = Day16.part1(input);
         assertEquals(expected, result);
     }
 
     @ParameterizedTest
-    @TestInput(input = "y2021/day16_sample.txt", expected = "1")
-    @TestInput(input = "y2021/day16.txt", expected = "1")
+    @TestInput(input = "y2021/day16_sample8.txt", expected = "3")
+    @TestInput(input = "y2021/day16_sample9.txt", expected = "54")
+    @TestInput(input = "y2021/day16_sample10.txt", expected = "7")
+    @TestInput(input = "y2021/day16_sample11.txt", expected = "9")
+    @TestInput(input = "y2021/day16_sample12.txt", expected = "1")
+    @TestInput(input = "y2021/day16_sample13.txt", expected = "0")
+    @TestInput(input = "y2021/day16_sample14.txt", expected = "0")
+    @TestInput(input = "y2021/day16_sample15.txt", expected = "1")
+    @TestInput(input = "y2021/day16.txt", expected = "1495959086337")
     void part2(List<String> input, long expected) {
         var result = Day16.part2(input);
         assertEquals(expected, result);
