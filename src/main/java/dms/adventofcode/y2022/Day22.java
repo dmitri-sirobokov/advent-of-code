e5fbcc7ced225f4c7387e4c97cc3d7e7ed33e412
dsirobokov <dsirobokov@anwb.nl>
2022-12-23 00:06:30 +0100

    Year 2022 / Day 22, Part 2 (Under construction)

--- src/main/java/dms/adventofcode/y2022/Day22.java
+++ src/main/java/dms/adventofcode/y2022/Day22.java
@@ -9,8 +9,20 @@ import java.util.List;
  */
 public class Day22 extends CodeBase {
 
-    public static long part1(List<String> input) {
+    public static long part1(List<String> input, int sampleType) {
+        return run(input, sampleType);
+    }
+
+    public static long run(List<String> input, int sampleType) {
         var mapData = readMapData(input);
+        if (sampleType == 0) {
+            part1SampleMapData(mapData);
+        } else if (sampleType == 1) {
+            part1RealMapData(mapData);
+        } else if (sampleType == 2) {
+            part2SampleMapData(mapData);
+        }
+
         var pos = move(mapData, new Position(0, 0, 0), 0);
 
         for (var i = 0; i < mapData.instructions.length(); i++) {
@@ -36,14 +48,165 @@ public class Day22 extends CodeBase {
                     }
                 }
                 pos = new Position(pos.x, pos.y, dir);
+                mapData.cube.dir = pos.dir;
             }
         }
 
         return 1000 * (pos.y + 1) + 4 * (pos.x + 1) + pos.dir;
     }
 
-    public static long part2(List<String> input) {
-        return 0;
+    private static void part1SampleMapData(MapData mapData) {
+        mapData.cube.front.x = 2 * mapData.cube.size;
+        mapData.cube.front.y = 0;
+
+        mapData.cube.bottom.x = 2 * mapData.cube.size;
+        mapData.cube.bottom.y = mapData.cube.size;
+
+        mapData.cube.left.x = mapData.cube.size;
+        mapData.cube.left.y = mapData.cube.size;
+
+        mapData.cube.top.x = 0;
+        mapData.cube.top.y = mapData.cube.size;
+
+        mapData.cube.back.x = 2 * mapData.cube.size;
+        mapData.cube.back.y = 2 * mapData.cube.size;
+
+        mapData.cube.right.x = mapData.cube.size;
+        mapData.cube.right.y = 3 * mapData.cube.size;
+
+        mapData.cube.front.left = new CubeEdge(2, mapData.cube.front, mapData.cube.front);
+        mapData.cube.front.right = new CubeEdge(0, mapData.cube.front, mapData.cube.front);
+        mapData.cube.front.top =  new CubeEdge(3, mapData.cube.front, mapData.cube.back);
+        mapData.cube.front.bottom = new CubeEdge(1, mapData.cube.front, mapData.cube.bottom);
+
+        mapData.cube.bottom.left = new CubeEdge(2, mapData.cube.bottom, mapData.cube.left);
+        mapData.cube.bottom.right = new CubeEdge(0, mapData.cube.bottom, mapData.cube.top);
+        mapData.cube.bottom.top = new CubeEdge(3, mapData.cube.bottom, mapData.cube.front);
+        mapData.cube.bottom.bottom = new CubeEdge(1, mapData.cube.bottom, mapData.cube.back);
+
+        mapData.cube.left.left = new CubeEdge(2, mapData.cube.left, mapData.cube.top);
+        mapData.cube.left.right = new CubeEdge(0, mapData.cube.left, mapData.cube.bottom);
+        mapData.cube.left.top = new CubeEdge(3, mapData.cube.left, mapData.cube.left);
+        mapData.cube.left.bottom = new CubeEdge(1, mapData.cube.left, mapData.cube.left);
+
+        mapData.cube.top.left = new CubeEdge(2, mapData.cube.top, mapData.cube.bottom);
+        mapData.cube.top.right = new CubeEdge(0, mapData.cube.top, mapData.cube.left);
+        mapData.cube.top.top = new CubeEdge(3, mapData.cube.top, mapData.cube.top);
+        mapData.cube.top.bottom = new CubeEdge(1, mapData.cube.top, mapData.cube.top);
+
+        mapData.cube.back.left = new CubeEdge(2, mapData.cube.back, mapData.cube.right);
+        mapData.cube.back.right = new CubeEdge(0, mapData.cube.back, mapData.cube.right);
+        mapData.cube.back.top = new CubeEdge(3, mapData.cube.back, mapData.cube.bottom);
+        mapData.cube.back.bottom = new CubeEdge(1, mapData.cube.back, mapData.cube.front);
+
+        mapData.cube.right.left = new CubeEdge(2, mapData.cube.right, mapData.cube.bottom);
+        mapData.cube.right.right = new CubeEdge(0, mapData.cube.right, mapData.cube.bottom);
+        mapData.cube.right.top = new CubeEdge(3, mapData.cube.right, mapData.cube.right);
+        mapData.cube.right.bottom = new CubeEdge(1, mapData.cube.right, mapData.cube.right);
+    }
+
+    private static void part2SampleMapData(MapData mapData) {
+        mapData.cube.front.x = 2 * mapData.cube.size;
+        mapData.cube.front.y = 0;
+
+        mapData.cube.bottom.x = 2 * mapData.cube.size;
+        mapData.cube.bottom.y = mapData.cube.size;
+
+        mapData.cube.left.x = mapData.cube.size;
+        mapData.cube.left.y = mapData.cube.size;
+
+        mapData.cube.top.x = 0;
+        mapData.cube.top.y = mapData.cube.size;
+
+        mapData.cube.back.x = 2 * mapData.cube.size;
+        mapData.cube.back.y = 2 * mapData.cube.size;
+
+        mapData.cube.right.x = mapData.cube.size;
+        mapData.cube.right.y = 3 * mapData.cube.size;
+
+        mapData.cube.front.left = new CubeEdge(1, mapData.cube.front, mapData.cube.left);
+        mapData.cube.front.right = new CubeEdge(0, mapData.cube.front, mapData.cube.right);
+        mapData.cube.front.top =  new CubeEdge(1, mapData.cube.front, mapData.cube.top);
+        mapData.cube.front.bottom = new CubeEdge(1, mapData.cube.front, mapData.cube.bottom);
+
+        mapData.cube.bottom.left = new CubeEdge(2, mapData.cube.bottom, mapData.cube.left);
+        mapData.cube.bottom.right = new CubeEdge(1, mapData.cube.bottom, mapData.cube.right);
+        mapData.cube.bottom.top = new CubeEdge(3, mapData.cube.bottom, mapData.cube.front);
+        mapData.cube.bottom.bottom = new CubeEdge(1, mapData.cube.bottom, mapData.cube.back);
+
+        mapData.cube.left.left = new CubeEdge(2, mapData.cube.left, mapData.cube.top);
+        mapData.cube.left.right = new CubeEdge(0, mapData.cube.left, mapData.cube.bottom);
+        mapData.cube.left.top = new CubeEdge(0, mapData.cube.left, mapData.cube.front);
+        mapData.cube.left.bottom = new CubeEdge(0, mapData.cube.left, mapData.cube.back);
+
+        mapData.cube.top.left = new CubeEdge(0, mapData.cube.top, mapData.cube.right);
+        mapData.cube.top.right = new CubeEdge(0, mapData.cube.top, mapData.cube.left);
+        mapData.cube.top.top = new CubeEdge(1, mapData.cube.top, mapData.cube.front);
+        mapData.cube.top.bottom = new CubeEdge(3, mapData.cube.top, mapData.cube.bottom);
+
+        mapData.cube.back.left = new CubeEdge(2, mapData.cube.back, mapData.cube.left);
+        mapData.cube.back.right = new CubeEdge(0, mapData.cube.back, mapData.cube.right);
+        mapData.cube.back.top = new CubeEdge(3, mapData.cube.back, mapData.cube.bottom);
+        mapData.cube.back.bottom = new CubeEdge(3, mapData.cube.back, mapData.cube.top);
+
+        mapData.cube.right.left = new CubeEdge(2, mapData.cube.right, mapData.cube.back);
+        mapData.cube.right.right = new CubeEdge(0, mapData.cube.right, mapData.cube.front);
+        mapData.cube.right.top = new CubeEdge(3, mapData.cube.right, mapData.cube.bottom);
+        mapData.cube.right.bottom = new CubeEdge(1, mapData.cube.right, mapData.cube.top);
+    }
+
+    private static void part1RealMapData(MapData mapData) {
+        mapData.cube.front.x = 1 * mapData.cube.size;
+        mapData.cube.front.y = 0;
+
+        mapData.cube.bottom.x = 1 * mapData.cube.size;
+        mapData.cube.bottom.y = mapData.cube.size;
+
+        mapData.cube.left.x = 0;
+        mapData.cube.left.y = 2 * mapData.cube.size;
+
+        mapData.cube.top.x = 0;
+        mapData.cube.top.y = 3 * mapData.cube.size;
+
+        mapData.cube.back.x = mapData.cube.size;
+        mapData.cube.back.y = 2 * mapData.cube.size;
+
+        mapData.cube.right.x = 2 * mapData.cube.size;
+        mapData.cube.right.y = 0;
+
+        mapData.cube.front.left = new CubeEdge(2, mapData.cube.front, mapData.cube.right);
+        mapData.cube.front.right = new CubeEdge(0, mapData.cube.front, mapData.cube.right);
+        mapData.cube.front.top = new CubeEdge(3, mapData.cube.front, mapData.cube.back);
+        mapData.cube.front.bottom = new CubeEdge(1, mapData.cube.front, mapData.cube.bottom);
+
+        mapData.cube.bottom.left = new CubeEdge(2, mapData.cube.bottom, mapData.cube.bottom);
+        mapData.cube.bottom.right = new CubeEdge(0, mapData.cube.bottom, mapData.cube.bottom);
+        mapData.cube.bottom.top = new CubeEdge(3, mapData.cube.bottom, mapData.cube.front);
+        mapData.cube.bottom.bottom = new CubeEdge(1, mapData.cube.bottom, mapData.cube.back);
+
+        mapData.cube.left.left = new CubeEdge(2, mapData.cube.left, mapData.cube.back);
+        mapData.cube.left.right = new CubeEdge(0, mapData.cube.left, mapData.cube.back);
+        mapData.cube.left.top = new CubeEdge(3, mapData.cube.left, mapData.cube.top);
+        mapData.cube.left.bottom = new CubeEdge(1, mapData.cube.left, mapData.cube.top);
+
+        mapData.cube.top.left = new CubeEdge(2, mapData.cube.top, mapData.cube.top);
+        mapData.cube.top.right = new CubeEdge(0, mapData.cube.top, mapData.cube.top);;
+        mapData.cube.top.top = new CubeEdge(3, mapData.cube.top, mapData.cube.left);
+        mapData.cube.top.bottom = new CubeEdge(1, mapData.cube.top, mapData.cube.left);
+
+        mapData.cube.back.left = new CubeEdge(2, mapData.cube.back, mapData.cube.left);
+        mapData.cube.back.right = new CubeEdge(0, mapData.cube.back, mapData.cube.left);
+        mapData.cube.back.top = new CubeEdge(3, mapData.cube.back, mapData.cube.bottom);
+        mapData.cube.back.bottom = new CubeEdge(1, mapData.cube.back, mapData.cube.front);
+
+        mapData.cube.right.left = new CubeEdge(2, mapData.cube.right, mapData.cube.front);
+        mapData.cube.right.right = new CubeEdge(0, mapData.cube.right, mapData.cube.front);
+        mapData.cube.right.top = new CubeEdge(3, mapData.cube.right, mapData.cube.right);
+        mapData.cube.right.bottom = new CubeEdge(1, mapData.cube.right, mapData.cube.right);
+    }
+
+    public static long part2(List<String> input, int dataType) {
+        return run(input, dataType);
     }
 
     private static Position move(MapData mapData, Position pos, int steps) {
@@ -51,13 +214,39 @@ public class Day22 extends CodeBase {
         var yStep = pos.dir == 1 ? 1 : pos.dir == 3 ? -1 : 0;
         var x = pos.x;
         var y = pos.y;
+        var dir = pos.dir;
+
         while (mapData.cube.map[y][x] == ' ') {
             x = x + xStep;
             y = y + yStep;
         }
+
         for (var i = 0; i < steps; i++) {
             var prevx = x;
             var prevy = y;
+            var prevDir = pos.dir;
+            var prevFace = mapData.cube.currentFace;
+
+            mapData.cube.x += xStep;
+            mapData.cube.y += yStep;
+            if (mapData.cube.x >= mapData.cube.currentFace.x + mapData.cube.size) {
+                mapData.cube.dir = mapData.cube.currentFace.right.dir;
+                mapData.cube.currentFace = mapData.cube.currentFace.right.dest;
+                mapData.cube.x = mapData.cube.currentFace.x;
+            } else if (mapData.cube.x < mapData.cube.currentFace.x) {
+                mapData.cube.dir = mapData.cube.currentFace.left.dir;
+                mapData.cube.currentFace = mapData.cube.currentFace.left.dest;
+                mapData.cube.x = mapData.cube.currentFace.x + mapData.cube.size - 1;
+            } else if (mapData.cube.y >= mapData.cube.currentFace.y + mapData.cube.size) {
+                mapData.cube.dir = mapData.cube.currentFace.bottom.dir;
+                mapData.cube.currentFace = mapData.cube.currentFace.bottom.dest;
+                mapData.cube.y = mapData.cube.currentFace.y;
+            } else if (mapData.cube.y < mapData.cube.currentFace.y) {
+                mapData.cube.dir = mapData.cube.currentFace.top.dir;
+                mapData.cube.currentFace = mapData.cube.currentFace.top.dest;
+                mapData.cube.y = mapData.cube.currentFace.y + mapData.cube.size - 1;
+            }
+
             x = (x + xStep) % mapData.cube.map[0].length;
             y = (y + yStep) % mapData.cube.map.length;
             if (x < 0) {
@@ -76,13 +265,26 @@ public class Day22 extends CodeBase {
                     y = mapData.cube.map.length + y;
                 }
             }
+
+            assert mapData.cube.x == x;
+            assert mapData.cube.y == y;
+            assert mapData.cube.dir == dir;
+
+            if (mapData.cube.map[mapData.cube.y][mapData.cube.x] == '#') {
+                mapData.cube.x = prevx;
+                mapData.cube.y = prevy;
+                mapData.cube.dir = prevDir;
+                mapData.cube.currentFace = prevFace;
+
+            }
             if (mapData.cube.map[y][x] == '#') {
                 x = prevx;
                 y = prevy;
+                dir = prevDir;
                 break;
             }
         }
-        return new Position(x, y, pos.dir);
+        return new Position(x, y, dir);
     }
 
     private static MapData readMapData(List<String> input) {
@@ -99,7 +301,6 @@ public class Day22 extends CodeBase {
 
         var cube = new Cube(map);
         return new MapData(cube, input.get(maxY + 1));
-
     }
 
     private static class MapData {
@@ -107,10 +308,10 @@ public class Day22 extends CodeBase {
         private final Cube cube;
         private final String instructions;
 
+
         public MapData(Cube cube, String instructions) {
             this.instructions = instructions;
             this.cube = cube;
-
         }
     }
 
@@ -120,10 +321,15 @@ public class Day22 extends CodeBase {
     private static class Face {
         private int x;
         private int y;
+        private CubeEdge left;
+        private CubeEdge right;
+        private CubeEdge top;
+        private CubeEdge bottom;
     }
 
     private static class Cube {
         private final char[][] map;
+        public int dir;
         private int size = 0;
 
         private int x;
@@ -135,6 +341,7 @@ public class Day22 extends CodeBase {
         private Face right = new Face();
         private Face front = new Face();
         private Face back = new Face();
+        private Face currentFace = front;
 
         public Cube(char[][] map) {
             this.map = map;
@@ -151,16 +358,13 @@ public class Day22 extends CodeBase {
                 size++;
             }
 
-            // starting point
-            var x0 = 0;
-            for (var x = 0; x < map[0].length; x++) {
-                if (map[0][x] != ' ') {
-                    x0 = x;
-                    break;
-                }
+            while (map[0][x] == ' ') {
+                x++;
             }
 
         }
 
     }
+
+    private record CubeEdge(int dir, Face source, Face dest) { }
 }
